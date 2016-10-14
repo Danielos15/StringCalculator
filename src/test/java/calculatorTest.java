d2af4a5399d41dbe4817ba849ea8f6d49a18a0a7
Daníel Örn <danielos15@ru.is>
2016-10-14 01:42:04 +0000

    TDD kata 1 done

--- src/test/java/calculatorTest.java
+++ src/test/java/calculatorTest.java
@@ -9,4 +9,16 @@ public class calculatorTest {
 	public void testEmptyString() {
 		assertEquals(0, calculator.add(""));
 	}
+	
+	@Test
+	public void testSingleNumber() {
+		assertEquals(1, calculator.add("1"));
+	}
+	
+	@Test
+	public void testTwoNumbers() {
+		assertEquals(3, calculator.add("1,2"));
+	}
+	
+	
 }
